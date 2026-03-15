35fad1ba2f6ef94e0549babd349e26fed8140537
sujal <saraswatsujal@gmail.com>
2026-03-16 00:39:03 +0530

    minor changes

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv'
 const connectDB = () => {
     try {
         mongoose.connect(process.env.MONGO_URL);
+        if(!process.env.MONGO_URL){
+            throw new Error("Mongo url variable is not present in dotenv file.");
+        }
         console.log("database connected...no error");
     } catch (error) {
         console.log("error:",error);
--- Backend/model/auth.model.js
+++ Backend/model/auth.model.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name : {
         type : String,
-        required : true
+        required : [true,"username is required"],
+        unique : [true,"username must be unique"]
     },
     email : {
         type : String,
-        required : true
+        required : [true,"email is required"],
+        unique : [true,"email must be unique"]
     },
     password : {
         type : String,
-        required : true
+        required : [true,"password is required"]
     }
 })
 
