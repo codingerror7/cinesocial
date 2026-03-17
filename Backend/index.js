87acab20aad055e13b3dbf013825804bf1f441fb
sujal <saraswatsujal@gmail.com>
2026-03-18 01:18:17 +0530

    minor update

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.config.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRouter);
 
