6f3591c8405132a86f2d1b90e592180aec87ade3
sujal <saraswatsujal@gmail.com>
2026-04-02 18:14:51 +0530

    updated auth middleware

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,21 +1,31 @@
+//Middleware = a gatekeeper that runs before your actual route logic
+
+//Instead of checking JWT inside every API, you create one reusable layer.
 import jwt from "jsonwebtoken";
 
-const authMiddleware = (req, res, next) => {
-  const header = req.headers.authorization;
+const authMiddleware = (req,res,next) => {
 
-  if (!header) {
-    return res.status(401).json({ message: "No token" });
-  }
+// If authentication succeeds → next()
+//If fails → send error response
 
-  const token = header.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Token expired" });
-  }
-};
-
+    const header = req.header.authorization;
+    //Reads the Authorization header from request.
+    if(!header){
+        return res.status(401).json({message : "user not authorized."});
+    }
+    //if no token is present in header than it signals unauthorized access and hence access is denied.
+    const token = header.split(" ")[1];
+    //it extracts token from header of request and seperates it with single spacing. here, [1] means token.
+    try {
+        const decode = jwt.verify(token,process.env.JWT_SECRET);
+        //comapres secret string presnt in token and dotenv, if valid then moves forward.
+        req.user = decode;
+        //now, attach extract user data from token like phone,userID and give it to decode. the authcontroller can directly use the user info and hence there is no need for it to again verify token.
+        next();
+        //moves forward.
+    } catch (error) {
+        return res.status(401).json({message:error});
+    }
+   
+}
 export default authMiddleware;
