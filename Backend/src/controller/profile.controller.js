d99ce485841d5fa1997632a9c9545a6ec4a2e725
sujal <saraswatsujal@gmail.com>
2026-06-03 18:05:12 +0530

    added redis to get profile

--- Backend/src/controller/profile.controller.js
+++ Backend/src/controller/profile.controller.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import redisClient from "../config/redis.config.js";
 import User from "../model/User.models.js";
+
 
 export const createProfile = async (req,res) => {
     try {
@@ -56,11 +58,28 @@ export const getProfile = async (req,res) => {
         if (!id) {
             return res.status(400).json({ message: "profile id required" });
         }
+        //redis caching implementation
+        const cachedKey = `profile:${id}`;
+        const cachedProfile = await redisClient.get(cachedKey);
+        if(cachedProfile){
+            try{
+                console.log("cache hit for profile data");
+                return res.status(200).json(JSON.parse(cachedProfile));
+            }
+            catch{
+                console.log("cache missed due to parsing error, fetching from db");
+            }
+        }
+
         const profile = await User.findById(id).select("-password");  //select is a mongoose query used to include and exclude the values, here "minus" sign is telling mongodb to exclude password field in the document and not to send password with response.
         if(!profile){
             return res.status(404).json({message : "profile not found"});
         }
+
+        await redisClient.setEx(cachedKey, 600, JSON.stringify(profile));  //caching the profile data in redis with an expiration time of 600 seconds (10 minutes)
+
         return res.status(200).json(profile);
+
     } catch (error) {
         return res.status(500).json({message : "failed to fetch user profile from db",error});
     }
